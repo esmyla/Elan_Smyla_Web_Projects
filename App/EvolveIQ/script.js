bab03f6bf000f494d878c07e8255eea83ba63cc8
elansmyla <esmyla@purdue.edu>
2025-09-16 03:04:33 -0400

    fix1 env erorr

--- App/EvolveIQ/script.js
+++ App/EvolveIQ/script.js
@@ -8,10 +8,14 @@
 // ────────────────────────────────
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 console.log(`API Key: ${SUPABASE_URL}`);
-const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 console.log(`API Key: ${SUPABASE_KEY}`);
+
+if (!SUPABASE_URL || !SUPABASE_KEY) {
+    console.error("Supabase environment variables are missing.");
+}
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
